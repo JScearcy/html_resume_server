892d2a4a4af0e1855437efe8bd0e5c0b52085db5
JScearcy <jacobscearcy@gmail.com>
2015-08-25 15:25:47 -0500

    Add readme

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
-
+//serve the index.html directly when the site is visited
 router.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../public/views/index.html'));
 });
--- server.js
+++ server.js
@@ -3,9 +3,10 @@ var app = express();
 var path = require('path');
 var routes = require('./routes/routes.js');
 
+//declare routes and open up the public folder
 app.use('/', routes);
 app.use(express.static(path.join(__dirname, './public')));
-
+//start the server and console log the port it is on.
 var server = app.listen(process.env.PORT || 3000, function(){
   console.log('Listening on port: ' + server.address().port);
 });
