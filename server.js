01444d263aca743c502414a5715ec0a4663896fe
JScearcy <jacobscearcy@gmail.com>
2015-08-25 21:09:47 -0500

    Move bootstrap, add gitignore

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var routes = require('./routes/routes.js');
 
 //declare routes and open up the public folder
 app.use('/', routes);
+app.use('/scripts', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/')))
 app.use(express.static(path.join(__dirname, './public')));
 //start the server and console log the port it is on.
 var server = app.listen(process.env.PORT || 3000, function(){
